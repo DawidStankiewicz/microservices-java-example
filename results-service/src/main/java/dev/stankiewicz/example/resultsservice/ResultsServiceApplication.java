e4e75f1865eed98107bd02aa1dec2d13afe26a18
Dawid Stankiewicz <dawid.stanki@gmail.com>
2024-08-17 18:45:17 +0200

    add results-service & discovery-service

---
+++ results-service/src/main/java/dev/stankiewicz/example/resultsservice/ResultsServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.stankiewicz.example.resultsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResultsServiceApplication.class, args);
+    }
+
+}
---
+++ results-service/src/test/java/dev/stankiewicz/example/resultsservice/ResultsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.stankiewicz.example.resultsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
